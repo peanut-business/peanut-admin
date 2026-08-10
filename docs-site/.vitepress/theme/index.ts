c93445f7cdc97a28a195849bc96a02f05cd7dbc3
xing.gu <905853860@qq.com>
2026-08-11 07:19:46 +0800

    fix(docs): route not-found through theme layout

--- docs-site/.vitepress/theme/index.ts
+++ docs-site/.vitepress/theme/index.ts
@@ -1,8 +1,11 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import NotFound from './NotFound.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
-  NotFound,
+  Layout: () => h(DefaultTheme.Layout!, null, {
+    'not-found': () => h(NotFound),
+  }),
 }
