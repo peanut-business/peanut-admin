e96d8b1f71b33131e081964e5619d580587fb399
xing.gu <905853860@qq.com>
2026-08-14 07:30:14 +0800

    chore: clear scaffold artifact before reseal

--- scaffold/releases/v1.1.0/files/server/config/platform_auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    // Dedicated HMAC material for platform login identifiers; fail closed when absent/short.
-    'identifier_hmac_key' => env('PLATFORM_IDENTIFIER_HMAC_KEY', ''),
-];
