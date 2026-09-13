ec78cc5c9f235c7d210a0ed8be740661f853d788
xing.gu <905853860@qq.com>
2026-09-13 17:50:30 +0800

    refactor(exception): use native framework reporting

--- server/app/ExceptionHandle.php
+++ server/app/ExceptionHandle.php
@@ -33,19 +33,6 @@ class ExceptionHandle extends Handle
     ];
 
     /**
-     * 记录异常信息（包括日志或者其它方式记录）
-     *
-     * @access public
-     * @param  Throwable $exception
-     * @return void
-     */
-    public function report(Throwable $exception): void
-    {
-        // 使用内置的方式记录异常日志
-        parent::report($exception);
-    }
-
-    /**
      * Render an exception into an HTTP response.
      *
      * @access public
